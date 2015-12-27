f662751e2dfce1792a1dbf56cb01657dee047bc0
Oskar Kaczmarzyk <oskarkaczmarzyk@outlook.com>
2015-12-27 19:58:43 +0100

    poprawka

--- Glass Witch/WprowadzanieNowegoKlienta.Designer.cs
+++ Glass Witch/WprowadzanieNowegoKlienta.Designer.cs
@@ -281,7 +281,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 596);
+            this.ClientSize = new System.Drawing.Size(644, 421);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
